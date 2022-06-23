37addaa23c2045350f274358224399343b968a79
ElenaR <llinusik@mail.ru>
2022-06-23 17:13:00 +0300

    Initial commit

---
+++ src/test/java/com/example/hw_2_3_1ver/Hw231verApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_3_1ver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw231verApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
