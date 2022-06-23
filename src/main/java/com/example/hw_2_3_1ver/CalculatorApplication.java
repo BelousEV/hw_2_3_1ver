af2e0b4f8e27d046bda13f3613ee85f7aff3c3a2
ElenaR <llinusik@mail.ru>
2022-06-23 18:38:38 +0300

    Первый коммит

--- src/main/java/com/example/hw_2_3_1ver/CalculatorApplication.java
+++ src/main/java/com/example/hw_2_3_1ver/CalculatorApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hw231verApplication {
+public class CalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hw231verApplication.class, args);
+
+        SpringApplication.run(CalculatorApplication.class, args);
     }
 
 }
